02e17eaa7c2bae05eef300da9b1b229e81aef7cd
Samuel <themainsamuel@gmail.com>
2024-06-29 02:30:22 +0100

    maybe now?

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # puppet script to install flask from pip3
 
-package {'Python':
-  ensure   => '3.8.10',
-}
 package {'Flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+package {'Python':
+  ensure   => '3.8.10',
 }
 package {'Werkzeug':
   ensure   => '2.1.1',
