0d0335bfc4a5fa56d9f16e9af21e1b658c567d8d
Samuel <themainsamuel@gmail.com>
2024-06-29 02:25:05 +0100

    modified task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # puppet script to install flask from pip3
 
+package {'python':
+  ensure   => '3.8.10',
+}
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip'
+}
+package {'Werkzeug':
+  ensure   => '2.1.1',
 }
