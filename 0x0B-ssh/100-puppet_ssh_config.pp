44bc631b5e796d60031f1e07d074390eadab406f
Samuel <themainsamuel@gmail.com>
2024-07-03 00:55:30 +0100

    I forget my quotes ever so often

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # doing task 2 with puppet
 
 file_line { 'configuring /etc/ssh/ssh_config':
-  path => /etc/ssh/ssh_config,
+  path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
 file_line { 'adding another line':
-  path =>  /etc/ssh/ssh_config,
+  path =>  '/etc/ssh/ssh_config',
   line => 'IdentityFile "~/.ssh/school"',
 }
