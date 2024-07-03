8b6706d534826e6900368687652874b8db8d3a4d
Samuel <themainsamuel@gmail.com>
2024-07-03 01:03:07 +0100

    work now?

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,12 @@
 # doing task 2 with puppet
 
 file_line { 'configuring /etc/ssh/ssh_config':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  ensure => 'present',
 }
 file_line { 'adding another line':
-  path =>  '/etc/ssh/ssh_config',
-  line => 'IdentityFile "~/.ssh/school"',
+  path   =>  '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  ensure => 'present',
 }
