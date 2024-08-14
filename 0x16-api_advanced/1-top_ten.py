809985418bdc0b3230cc30d5bd954100391ccf5b
Samuel <themainsamuel@gmail.com>
2024-08-15 00:41:38 +0100

    added the pinned posts...removed the [2:] and made the limit 8

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,11 +9,11 @@ def top_ten(subreddit):
     """
     url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
     header = {'user-agent': 'chrome'}
-    param = {'limit': 10}
+    param = {'limit': 8}
     res = req.get(url, allow_redirects=False, headers=header, params=param)
     if res.status_code == 200:
         data = res.json()
-        for post in data["data"]["children"][2:]:
+        for post in data["data"]["children"]:
             print(f"{post['data']['title']}")
     else:
         print(None)
