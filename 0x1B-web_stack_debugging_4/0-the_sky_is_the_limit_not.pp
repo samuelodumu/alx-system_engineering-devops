97adce7d9de264d8f2bc55199a44d5fb2dbd98e6
Samuel <themainsamuel@gmail.com>
2024-08-20 11:26:57 +0100

    added sudo to sed command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # increases worker processes and restarts nginx
 
 exec {'increase_worker_processes':
-  command => "sed -i 's/worker_connections.*/worker_connections 1024;/' /etc/nginx/nginx.conf",
-  onlyif  => "grep -q 'worker_connections 1024;' /etc/nginx/nginx.conf",
-  path    => ['/bin', '/usr/bin']
+  command => "sudo sed -i 's/worker_connections.*/worker_connections 1024;/' /etc/nginx/nginx.conf",
+  onlyif  => "sudo grep -q 'worker_connections 1024;' /etc/nginx/nginx.conf",
+  path    => ['/bin', '/usr/bin'],
   notify  => Exec['restart_nginx'],
 }
 
