6fd1e2a111fadbc1d834f77f39061f40ea2c6100
Samuel <themainsamuel@gmail.com>
2024-08-21 05:05:07 +0100

    updated path

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,11 +3,11 @@
 # increase hard file limit
 exec { 'increase_hard_file_limit':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  path    => ['/usr/local/bin/', '/bin/']
 }
 
 # increase soft file limit
 exec { 'increase_soft_file_limit':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  path    => ['/usr/local/bin/', '/bin/']
 }
